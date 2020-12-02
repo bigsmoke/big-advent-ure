7965c15857f56ce0d2f8cb9c0283f1201caf72c0
Rowan Rodrik van der Molen <rowan@ytec.nl>
2020-12-02 18:16:27 +0100

    Day 1 progress

--- day01.c/main.c
+++ day01.c/main.c
@@ -1,16 +1,45 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 
 #define BUFFER_SIZE 1024
 
 
-int main(int argc, char* argv[])
+typedef struct {
+    int *array;
+    size_t used;
+    size_t size;
+} Expenses;
+
+void expenses_list_init(Expenses *expenses, size_t initial_size)
 {
+    expenses->array = malloc(initial_size * sizeof(int));
+    expenses->used = 0;
+    expenses->size = initial_size;
+}
+
+void expenses_list_append(Expenses *expenses, int expense)
+{
+    if (expenses->used == expenses->size)
+    {
+        expenses->size += 1;
+        expenses->array = realloc(expenses->array, expenses->size * sizeof(int));
+    }
+    expenses->array[expenses->used++] = expense;
+}
+
+
+int main(int argc, char *argv[])
+{
+    Expenses expenses;
+
+
     char buffer[BUFFER_SIZE];
 
     while (read(STDIN_FILENO, buffer, BUFFER_SIZE) != 0)
     {
+
     }
 }
 
