1d2d027a211845a07252b4aaecc0fcd123e09469
Rowan Rodrik van der Molen <rowan@ytec.nl>
2020-12-03 22:00:43 +0100

    Working C code for Day 1, Part1

--- day01.c/main.c
+++ day01.c/main.c
@@ -1,46 +1,142 @@
+#include <errno.h>
+#include <limits.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 
 #define BUFFER_SIZE 1024
+#define EXPENSE_DIGITS 16
+#define SOUGHT_SUM 2020
 
+
+typedef long ExpenseValue;
+typedef size_t LineNumber;
+
+typedef struct Expense {
+    struct Expense *next_greater_expense;
+    ExpenseValue value;
+    LineNumber line_number;
+} Expense;
 
 typedef struct {
-    int *array;
-    size_t used;
-    size_t size;
-} Expenses;
+    Expense *smallest_expense;
+    Expense *greatest_expense;
+    size_t num_expenses;
+} OrderedExpenses;
 
-void expenses_list_init(Expenses *expenses, size_t initial_size)
+void expenses_list_init(OrderedExpenses *expenses, size_t initial_size)
 {
-    expenses->array = malloc(initial_size * sizeof(int));
-    expenses->used = 0;
-    expenses->size = initial_size;
+    expenses->num_expenses = 0;
+    expenses->smallest_expense = NULL;
+    expenses->greatest_expense = NULL;
 }
 
-void expenses_list_append(Expenses *expenses, int expense)
+void expenses_list_add(OrderedExpenses *expenses, ExpenseValue new_value, LineNumber line_number)
 {
-    if (expenses->used == expenses->size)
+    Expense *current_expense = expenses->smallest_expense;  // Will be NULL if the list is empty.
+    Expense *previous_expense = NULL;
+    Expense *new_expense = (Expense*)malloc(sizeof(Expense));
+    new_expense->value = new_value;
+    new_expense->line_number = line_number;
+
+    // Get the `current_expense` pointer to point to the first expense in the list with a value greater than
+    // or equal to the value that we're adding:
+    while (current_expense != NULL)
     {
-        expenses->size += 1;
-        expenses->array = realloc(expenses->array, expenses->size * sizeof(int));
+        if (current_expense->value >= new_value) {
+            break;
+        }
+        previous_expense = current_expense;
+        current_expense = current_expense->next_greater_expense;
     }
-    expenses->array[expenses->used++] = expense;
+
+    if (previous_expense != NULL)
+    {
+        // If there is a smaller expense than our newly added expense,
+        // we insert ourselves in the list _after_ that expense.
+        previous_expense->next_greater_expense = new_expense;
+    }
+    else
+    {
+        // If there's no smaller expense, we put ourselves at the start of the list.
+        expenses->smallest_expense = new_expense;
+    }
+
+    new_expense->next_greater_expense = current_expense;
+
+    if (current_expense == NULL)
+    {
+        // We found no existing expense greater in or equal to the new expense.
+        expenses->greatest_expense = new_expense;
+    }
+
+    expenses->num_expenses++;
 }
 
+char stdin_buffer_of_max_digit_size[EXPENSE_DIGITS];
 
 int main(int argc, char *argv[])
 {
-    Expenses expenses;
+    OrderedExpenses expenses;
+    ExpenseValue value;
+    char *digits;
+    char *digits_end_ptr;
+    int num_bytes_read;
+    LineNumber line_number = 0;
 
+    expenses_list_init(&expenses, 10);
 
-    char buffer[BUFFER_SIZE];
+    digits = malloc(EXPENSE_DIGITS);
 
-    while (read(STDIN_FILENO, buffer, BUFFER_SIZE) != 0)
+    setvbuf(stdin, stdin_buffer_of_max_digit_size, _IOLBF, EXPENSE_DIGITS);
+
+    while ((num_bytes_read = read(STDIN_FILENO, digits, EXPENSE_DIGITS)) != 0)
     {
+        line_number++;
+        errno = 0;
+        value = strtol(digits, &digits_end_ptr, 10);
+        if ((errno == ERANGE && (value == LONG_MAX || value == LONG_MIN))
+                || (errno != 0 && value == 0))
+        {
+            perror("strtol()");
+            exit(EXIT_FAILURE);
+        }
+        if (digits_end_ptr == digits)
+        {
+            fprintf(stderr, "No digits were found\n");
+            exit(EXIT_FAILURE);
+        }
 
+        expenses_list_add(&expenses, value, line_number);
     }
+
+    Expense* expense_i = expenses.smallest_expense;
+    Expense* expense_j;
+    do
+    {
+        expense_j = expenses.smallest_expense;
+        do
+        {
+            if ((expense_i->value + expense_j->value) == SOUGHT_SUM)
+            {
+                fprintf(
+                    stdout, "%d = %ld [line %ld] + %ld [line %ld]\n%ld * %ld = %ld\n",
+                    SOUGHT_SUM,
+                    expense_i->value, expense_i->line_number,
+                    expense_j->value, expense_j->line_number,
+                    expense_i->value, expense_j->value, expense_i->value * expense_j->value
+                );
+                exit(EXIT_SUCCESS);
+            }
+        }
+        while ((expense_j = expense_j->next_greater_expense) != NULL);
+    }
+    while ((expense_i = expense_i->next_greater_expense) != NULL);
+
+    fprintf(stderr, "No 2 expenses found that sum to %i\n", SOUGHT_SUM);
+    exit(EXIT_FAILURE);
 }
 
 
